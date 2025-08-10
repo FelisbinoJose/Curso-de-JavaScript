5684bbbf36f3ade605a219a7d9a2b133f33c0603
FelisbinoJose <246760.jose@unibave.net>
2025-08-10 16:26:36 -0300

    feat (exercicios): Adiciona exercicio sobre a aula2

---
+++ aula2/exercicios/index.js
@@ -0,0 +1,4 @@
+//Escreva no consolo.log a seguinte frase:
+// Meu nome é "NOME". Estou aprendenfo JavaScript ás 10 da manhã.
+
+console.log('Meu nome é "José Luiz". Estou aprendendo JavaScript ás', 10, 'da manhã')
